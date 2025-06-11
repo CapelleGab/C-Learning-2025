6fa588cca958147634ef08cc287dd11b41e56c88
CapelleGab <gabin.capelle.pro@gmail.com>
2025-06-11 21:11:41 +0200

    P1 - Finish

--- Calculatrice/main.c
+++ Calculatrice/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello \n");
+  // fonction fgets()  // Lire l'entrée utilisateur
+  // fonction sscanf() // Analyse l'entrer
+  char input[100];
+  double num1, num2, result;
+  char operator;
+
+  printf("Entrez une expression (ex: 3 + 4) : ");
+  fgets(input, sizeof(input), stdin);
+
+  sscanf(input, "%lf %c %lf", &num1, &operator, &num2);
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+
+    case '-':
+      result = num1 - num2;
+      break;
+    
+    case '*':
+      result = num1 * num2;
+      break;
+    
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+      } else {
+        printf("Erreur : Division par zéro. \n");
+        return 1;
+      }
+      break;
+
+    default:
+      printf("Opérateur non valide.\n");
+      return 1;
+  }
+
+  printf("Le résultat est : %.2f\n", result);
   return 0;
 }
