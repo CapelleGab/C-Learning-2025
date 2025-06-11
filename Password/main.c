b101854a751c9a50f46893b5ca1acee0b818d96f
CapelleGab <gabin.capelle.pro@gmail.com>
2025-06-11 21:39:10 +0200

    P2 - Finish

--- Password/main.c
+++ Password/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ask_lenght_password(); 
+char *generate_password(int longueur);
 
 int main(void) {
-  printf("PasswordGenerator \n");
+  int lenght_password = ask_lenght_password();
+  char *password = generate_password(lenght_password);
+  printf("Le mot de passe est : %s\n", password);
   return 1;
+}
+
+int ask_lenght_password() {
+  char input[10];
+  int lenght;
+
+  printf("Saississez une longueur de mot de passe (1, 2, 5, ...) \n");
+  
+  fgets(input, sizeof(input), stdin);
+  lenght = atoi(input);
+
+  return lenght;
+}
+
+char *generate_password(int longueur) {
+  static char password[100];
+
+  for (int i = 0; i < longueur; i++) {
+    password[i] = 'a' + (rand() % 26);
+  }
+
+  password[longueur] = '\0';
+
+  return password;
 }
